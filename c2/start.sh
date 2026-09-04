abf286d6cb88c62190e81a1c5f49e433c8f48d7e
SurendraS26 <surendrasonusep26@gmail.com>
2026-09-04 21:18:22 +0530

    repo: Modify start.sh c2

--- c2/start.sh
+++ c2/start.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
-# This is the port number , don't edit this man
+
+# Shitty colors
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[0;37m'
+
+# This is the port number , don't edit this boy/girl
+
 TPMSERVE="2321"
 TPMCTRL="2322"
+
 # Startup script (visual)
+
 echo "[*] Starting swtpm-arch-container...."
 echo "[*] TPM running ip: 0.0.0.0"
 echo "server --> 0.0.0.0 , PORT=$TPMSERVE"
 echo "ctrl   --> 0.0.0.0 , PORT=$TPMCTRL"
-# Run swtpm (Own version is better than slop i feel that)
+
+# Run swtpm (Own version is better than slop i feel alva)
+
 /usr/bin/swtpm \
 	socket \
 	--tpm2 \
