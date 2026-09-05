a239a3895b145c889b2bed3f7ecd82ba9cfb5a6a
SurendraS26 <surendrasonusep26@gmail.com>
2026-09-05 20:45:09 +0530

    repo: fix issue in run.sh c2

--- c2/src/run.sh
+++ c2/src/run.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 echo "Started SWTPM"
-docker run -d --name trustedge-c2 \
-  --network trustedge-net \
-  -p 2321:2321 -p 2322:2322 \
-  -v swtpm-state:/var/lib/swtpm/tpmstate \
-  trustedge-c2
+docker run --network trustedge-net -p 2321:2321 -p 2322:2322 -v swtpm-state:/var/lib/swtpm/tpmstate trustedge-c2
 echo "Stopped the container"
