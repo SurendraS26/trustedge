38818942ce5421e0b1e56a70a4e68910889dc4ee
SurendraS26 <surendrasonusep26@gmail.com>
2026-09-08 19:33:30 +0530

    repo: fixed most of the things

---
+++ c1/scripts/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "[*] starting trustedge-c1"
+docker run -p 2321-2322:2321-2322 trustedge-c1
--- c1/start.sh
+++ c1/start.sh
@@ -10,13 +10,15 @@ NC='\033[0m'
 TPMSERVE="2321"
 TPMCTRL="2322"
 TPM2TOOLS_TCTI="swtpm:host=trustedge-c2,port=2321"
-echo "Developed by **SurendraS26** (-_-) "
-echo "Checkout: https://github.com/SurendraS26/swtpm-arch-docker"
-echo "[*] Starting swtpm Arch container"
-echo "[*] TPM will running in these below ip's only"
+echo -e "${NC}Developed by ${BOLD}SurendraS26${NC} ${YELLOW}(-_-)${NC}"
+echo -e "${BLUE}Checkout${NC} https://github.com/SurendraS26/swtpm-arch-docker"
+echo -e ""
+echo -e "${CYAN}[*]${NC} Starting swtpm Arch container"
+echo -e "${CYAN}[*]${NC} TPM will running in these below ip's only"
 ip -br addr show
-echo "server --> PORT=$TPMSERVE"
-echo "ctrl   --> PORT=$TPMCTRL"
+echo -e "${GREEN}server -->${NC} PORT=$TPMSERVE"
+echo -e "${GREEN}ctrl   -->${NC} PORT=$TPMCTRL"
+echo -e "${CYAN}To stop container${CYAN} ${NC}'${BOLD}docker stop <eg: trustedge-c1>${BOLD}'${NC}"
 /usr/bin/swtpm \
 	socket \
 	--tpm2 \
